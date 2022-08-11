6f5f94cea30c4e93d2df10872e345e3253712886
Raysson Ornelas <rayssonornelas2006@gmail.com>
2022-08-11 17:42:47 -0300

    fea: set background

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const CustomDocument = () => {
+  return (
+    <Html>
+      <Head />
+      <body className="bg-slate-800 text-white">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default CustomDocument;
